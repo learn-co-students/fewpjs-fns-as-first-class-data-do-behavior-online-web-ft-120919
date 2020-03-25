4d4b93a60eec9cbee240be52b752e026fdf6fad8
github username <meliajm@gmail.com>
2020-03-25 09:55:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-/* Given Code, don't edit */
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
 
-/* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  if time 
+  
+}
