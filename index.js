e44d08357ebd4da9c5fe7957c79690c377e2def1
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-26 16:24:51 -0700

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let timeSplit = time.split(":")
+  let hour = parseInt(timeSplit[0])
+  let min = parseInt(timeSplit[1])
+  if (hour < 12) {
+    return "Good Morning"
+  }
+  else if (hour >= 12 && hour <= 17) {
+    return "Good Afternoon"
+  }
+  else if (hour > 17) {
+    return "Good Evening"
+  }
+}
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  document.querySelector("#greeting").innerText = string
+}
