5ecea3494b7edc912edb8abe193d4ecbbf917f61
Noah Scala <nwscala@gmail.com>
2020-03-26 10:55:56 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let hourString = timeString.split(':')[0];
+  let timeNum = parseInt(hourString, 10);
+
+  if (timeNum < 12) {
+    return "Good Morning"
+  } else if (timeNum < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(displayString) {
+  const greetingHeader = document.querySelector('h1#greeting')
+  greetingHeader.innerText = displayString
+}
