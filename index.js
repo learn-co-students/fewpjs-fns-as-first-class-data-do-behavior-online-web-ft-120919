63a6dac96de83f82d5cc8bda153ba72fab75320d
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-03-27 07:03:42 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,28 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(string){
+  const hhAndMm = string.split(":");
+  const hh = parseInt(hhAndMm[0]);
+  if (hh < 12){
+    return "Good Morning"
+  }
+if (hh <= 17){
+  return "Good Afternoon"
+}
+if (hh > 17){
+  return "Good Evening"
+}
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string){
+  
+  let content = document.getElementById("greeting");
+  content.innerText = string;
+
+}
+      
+
