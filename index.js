c980f9db5cebb01f9dee962ded5089b5a950d328
mumu2117 <jmillerarcher@me.comgit config --global user.name mumu2117>
2020-04-01 21:01:14 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string){
+  let parsed = parseInt(string, 10);
+
+    if (parsed < 12){
+    return "Good Morning";
+  } else if (parsed < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(string){
+  let input = document.getElementById("greeting")
+  // input.addEventListener('click', function(event){
+  //   display = document.createElement('h1')
+  //   display.innerText =
+    input.innerText = string
+}
+
 /* Write your implementation of displayMessage() */
